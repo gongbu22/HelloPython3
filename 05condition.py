79cc5c930f278e48f28284303f30f2bc0ff1ed63
gongbu22 <pyujin0711@naver.com>
2024-07-12 12:17:06 +0900

    연습문제 풀이 추가

--- 05condition.py
+++ 05condition.py
@@ -164,13 +164,214 @@ match int(avg/10):
 print(grade)
 
 # 속도 위반 경고
+speed = int(input('자동차의 현재 속도는 : '))
+
+warn = ''
+if speed >= 50:
+    warn = '속도위반!!'
+
+print(warn)
+
 # 자동온도 조절 장치
+temp = int(input('기계 온도를 입력하세요. '))
+
+msg = '팬(Fan) 중지'
+if temp >= 40:
+    msg = '팬(Fan) 가동'
+
+print(msg)
+
 # 자동 주문 시스템
+menuIntro = '''
+Good morning. Nice to meet you.
+Where are you from?
+Please select a number
+1.대한민국 2.USA 3.中國 
+'''
+
+menu = input(menuIntro)
+msg = 'Would you like to order'
+
+match menu:
+    case '1': msg = '주문하시겠어요?'
+    case '3': msg = '您要点菜吗?'
+
+print(msg)
+
+#---------선생님 풀이-----------
+menuIntro = '''
+Good morning. Nice to meet you.
+Where are you from?
+Please select a number
+1.대한민국 2.USA 3.中國 
+'''
+
+menu = int(input(menuIntro))
+msg = 'Would you like to order'
+
+if menu == 1:
+    msg = '주문하시겠어요'
+elif menu == 3:
+    msg = '您要点菜吗?'
+
+print(msg)
+
 # 국가 재난지원금 수령액 조회
+family = int(input('인원수를 입력하세요. '))
+result = ''
+
+match family:
+    case 1: result = '400,000원'
+    case 2: result = '600,000원'
+    case 3: result = '800,000원'
+    case 4: result = '1,000,000원'
+
+print(result)
+
+#--------------------------------
+family = int(input('인원수를 입력하세요. '))
+result = ''
+
+if family == 1:
+    result = '400,000원'
+elif family == 2:
+    result = '600,000원'
+elif family == 3:
+    result = '800,000원'
+elif family == 4:
+    result = '1,000,000원'
+
+print(result)
+
 # 개선 BMI 지수 출력
+bmi = float(input('BMI 지수를 입력하세요 : '))
+result = '저체중'
+
+if bmi > 140: result = '고도 비만'
+elif bmi > 120: result = '비만'
+elif bmi > 110: result = '과체중'
+elif bmi > 90: result = '정상 체중'
+
+print(f'BMI 지수 : {bmi}, 진단 결과 : {result} 입니다.')
+
 # 버스 전용차로 단속
+msg1 = ''' 
+1.월~금, 2.토요일, 3.공휴일
+요일을 선택하세요. 
+'''
+msg2 = '''
+버스 전용차로 단속 중입니다
+1.버스, 2.승용차
+차종을 선택하세요. 
+'''
+msg3 = '버스 전용차로 위반!'
+msg4 = '버스입니다.'
+msg5 = '토요일 및 공휴일은 단속하지 않습니다.'
+
+dayweek = int(input(msg1))
+
+result = ''
+
+if dayweek == 1:    # 평일여부 확인
+    carType = int(input(msg2))
+    if carType != 1:    # 차량 종류 확인
+        result = msg3
+    else:
+        result = msg4
+else:
+    result = msg5
+
+print(result)
+
+
 # 마스크 구매 가능 요일 출력
+endBirthYear = int(input('출생연도 끝자리 입력 : '))
+age = int(input('만 나이 입력 : '))
+
+result = ''
+# if age < 65:
+#     if endBirthYear == 1 or endBirthYear == 6:
+#         result = '월요일'
+#     elif endBirthYear == 2 or endBirthYear == 7:
+#         result = '화요일'
+#     elif endBirthYear == 3 or endBirthYear == 8:
+#         result = '수요일'
+#     elif endBirthYear == 4 or endBirthYear == 9:
+#         result = '목요일'
+#     elif endBirthYear == 5 or endBirthYear == 0:
+#         result = '금요일'
+# else:
+#     result = '언제든지'
+
+match age:
+    case x if x < 65:
+        match endBirthYear:
+            case 1 | 6:
+                result = '월요일'
+            case 2 | 7:
+                result = '화요일'
+            case 3 | 8:
+                result = '수요일'
+            case 4 | 9:
+                result = '목요일'
+            case 5 | 0:
+                result = '금요일'
+    case _:
+        result = '언제든지'
+
+print(f'{result} 구매 가능합니다.')
 
 # 차량 2부제
+# day = int(input('오늘 날짜의 일은? '))
+from datetime import datetime
+day = datetime.today().day
+car = int(input('차량 번호 4자리를 입력하세요. '))
+
+ms1 = '오늘 입차: 번호가 홀수인 차량'
+ms2 = '귀하의 차량은 입차 불가합니다.'
+if day % 2 == 0:
+    ms1 = '오늘 입차: 번호가 짝수인 차량'
+if car % 2 == day % 2:
+    ms2 = '귀하의 차량은 입차 가능합니다.'
+
+print(f'''
+오늘 날짜: {day} 일
+{ms1}
+{ms2}
+''')
+
 # 생존율 출력
+msg1 = '최초 장비를 사용하기까지 걸린 시간(초)를 입력하세요. '
+time = int(input(msg1))
+
+result = '25% 미만'
+if time <= 60: result = '85%'
+elif time <= 120: result = '76%'
+elif time <= 180: result = '66%'
+elif time <= 240: result = '57%'
+elif time <= 300: result = '47%'
+elif time <= 360: result = '35%'
+
+print(f'생존율 : {result}')
+
 # 전기 요금 계산기
+basePrice = 910   # 기본요금
+unitPrice = 99.3   # 단가
+powerPrice = 0  # 전기요금
+
+powerAmount = int(input('전기 사용량을 입력하세요. '))
+if powerAmount > 400:
+    basePrice = 7300
+    unitPrice = 280.6
+elif powerAmount > 200:
+    basePrice = 1600
+    unitPrice = 187.6
+
+powerPrice = basePrice + (unitPrice * powerAmount)
+
+print(f'''
+🔌사용량 : {powerAmount} kwh
+기본요금: {basePrice} 원
+단가 : {unitPrice} 원
+⚡전기 요금 : {powerPrice} 원
+''')
