32e3ee028194e65aee7c2a097077969f5093a2fd
gongbu22 <pyujin0711@naver.com>
2024-07-16 15:10:08 +0900

    실습문제 풀이 보완 - 문자열 슬라이스

--- 14listslice.py
+++ 14listslice.py
@@ -29,3 +29,32 @@ gender = jumin[7:8]
 print(birth)
 print(gender)
 
+# 33
+cardno = input('카드번호는? ')
+
+result = '잘못된 카드 번호입니다.'
+if cardno[:2] == '35':
+    if cardno == 356317: result += 'JCB카드 NH농협카드'
+    elif cardno == 356901: result += 'JCB카드 신한카드'
+    elif cardno == 356912: result += 'JCB카드 KB국민카드'
+elif cardno[:1] == '4':
+    if cardno == 404825: result += '비자카드 비씨카드'
+    elif cardno == 438676: result += '비자카드 신한카드'
+    elif cardno == 457973: result += '비자카드 국민은행'
+elif cardno[:1] == '5':
+    if cardno == 515594: result += '마스타카드 신한카드'
+    elif cardno == 524353: result += '마스타카드 외환카드'
+    elif cardno == 540926: result += '마스타카드 국민은행'
+print(result)
+
+if cardno == 356317: result = 'JCB카드 NH농협카드'
+elif cardno == 356901: result = 'JCB카드 신한카드'
+elif cardno == 356912: result = 'JCB카드 KB국민카드'
+elif cardno == 404825: result = '비자카드 비씨카드'
+elif cardno == 438676: result = '비자카드 신한카드'
+elif cardno == 457973: result = '비자카드 국민은행'
+elif cardno == 515594: result = '마스타카드 신한카드'
+elif cardno == 524353: result = '마스타카드 외환카드'
+elif cardno == 540926: result = '마스타카드 국민은행'
+
+print(f'{cardno} / {result}')
