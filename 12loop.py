4ae727409fb7a44c5932ef0ffae52d725222e70c
gongbu22 <pyujin0711@naver.com>
2024-07-16 10:41:05 +0900

    연습문제 추가 break

--- 12loop.py
+++ 12loop.py
@@ -76,11 +76,27 @@ while i < 101:
 print(f'{result}, 최소공배수: {3*8}')
 
 # 삼각형 너비 계산하기
-i = 1
-j = 1
+i = 2
+j = 3
 area = 0
-while i * j <= 150:
+while i < 100:
+    area = (i * j)/2
+    i += 2
+    j += 3
+    if area > 150: break
+    print(f'삼각형의 넓이: {area}, i, j')
 
+# ----------선생님 풀이-----------
+limitArea = 150 # 반복 중단 삼각형 너비
+width = 2
+height = 3
+i = 1
+
+while True:
+    area = ((width * i) * (height * i)) / 2
+    if area > limitArea: break
+    print(f'삼각형의 너비 : {width * i} / {height * i} = {area}')
+    i += 1
 
 
 # 3 6 9 게임 (while로 작성)
@@ -99,5 +115,80 @@ while i < 100:
     i += 1
 
 # 열차 교차 시간 알아내기
+trainA = 10
+trainB = 25
+trainC = 30
+
+i = 1
+timeHour = 0
+timeMin = 0
+result = ''
+while i < 541:
+    timeHour = 9+(i // 60)
+    timeMin = i % 60
+    result = f'{timeHour}시 {timeMin}분'
+    if (i % trainA == 0) & (i % trainB == 0) & ( i % trainC == 0 ):
+        print(result)
+    elif (i % trainA == 0) & (i % trainB == 0):
+        print(result)
+    elif (i % trainB == 0) & ( i % trainC == 0 ):
+        print(result)
+    elif (i % trainA == 0) & ( i % trainC == 0 ):
+        print(result)
+    i += 1
+
+# ----------선생님 풀이---------------
+trainA = 10
+trainB = 25
+trainC = 30
+min = 1
+
+while min < 541:
+    if min % trainA == 0 and min % trainB == 0:
+        hour = 9 + min // 60
+        min = min % 60
+        print(f'{hour}시 {min}분 : A - B 교차!')
+    elif min % trainB == 0 and min % trainC == 0:
+        hour = 9 + min // 60
+        min = min % 60
+        print(f'{hour}시 {min}분 : B - C 교차!')
+    elif min % trainA == 0 and min % trainC == 0:
+        hour = 9 + min // 60
+        min = min % 60
+        print(f'{hour}시 {min}분 : A - C 교차!')
+    min += 1
+
 
 # 로그인 기능 만들기
+passwd = input('관리자 암호를 입력하세요. ')
+
+isLogin = False
+i = 1
+while i < 5:
+    if not isLogin:
+        passwd = input('관리자 암호를 입력하세요. ')
+
+        if passwd == 'hanbitac':
+            isLogin = True
+            print('로그인 됐습니다!')
+        else:
+            print('암호를 다시 확인하세요!')
+    i += 1
+if not isLogin: print('로그인 실패! 횟수 초과!')
+
+# ----------선생님 풀이------------
+cntLogin = 1
+while True:
+    passwd = input('관리자 암호를 입력하세요. ')
+
+    if passwd == 'hanbitac':
+        print('로그인 되었습니다!')
+        break
+    else:
+        print('암호를 다시 확인하세요!')
+
+    if cntLogin < 5: cntLogin += 1
+    else:
+        print('로그인 실패! 횟수 초과!')
+        break
+
