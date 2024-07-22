5d0a5e7b618c6667369673eaf2f27cc30f16c316
gongbu22 <pyujin0711@naver.com>
2024-07-22 17:08:37 +0900

    데이터베이스 프로그래밍 - sqlite3 - 상세조회, 삭제

--- 27sqlite3.py
+++ 27sqlite3.py
@@ -100,3 +100,44 @@ print(result)
 # 6
 cursor.close()
 conn.close()
+
+# 파이썬으로 데이터베이스 다루기 5 - 데이터 상세조회
+# 1
+conn = sqlite3.connect('db/python.db')
+
+# 2
+cursor = conn.cursor()
+
+# 3
+sql = 'select * from member where userid = ?'
+params = ('111', )
+
+# 4
+cursor.execute(sql, params)
+rs = cursor.fetchone()   # 결과가 여러개라면 fetchall()
+
+# 5
+print(f'{rs[0]} {rs[1]} {rs[2]} {rs[3]}')
+
+# 6
+cursor.close()
+conn.close()
+
+# 파이썬으로 데이터베이스 다루기 6 - 데이터 삭제
+# 1
+conn = sqlite3.connect('db/python.db')
+
+# 2
+cursor = conn.cursor()
+
+# 3
+sql = 'delete from member where userid = ?'
+params = ('abc123', )
+
+# 4
+cursor.execute(sql, params)
+print(cursor.rowcount, '건의 데이터가 삭제됨!!')
+conn.commit()
+
+cursor.close()
+conn.close()
